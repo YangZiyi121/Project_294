3ef06357cbbe650a7481443ce1aa2d67b6a2e038
YangZiyi121 <ziyi.yang@kaust.edu.sa>
2022-11-16 15:56:37 +0300

    for testing

--- Device.h
+++ Device.h
@@ -5,7 +5,7 @@ class Device {
 public:
 //For declaration in .h file, there should be no virtual in front of the function
 	void do_function();
-    
+    void clock_recieve();
 };
 
 #endif
--- Latch.cpp
+++ Latch.cpp
@@ -1,3 +1,4 @@
+#include "Latch.h"
 long long Latch::receive_clock() { 
     after = before; 
     return after;
