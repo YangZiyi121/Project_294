3087bca11a5d91be27ed63879c5df31ed7ff6868
rozirr <106530855+rozirr@users.noreply.github.com>
2022-11-17 14:16:09 +0300

    modified and tested twos complement

---
+++ Twos_complement.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Latch.h"
+#include "Device.h"
+#include "Port.h"
+
+class Twos_complement : Device
+{
+    public:
+    static const int cycles = 1;
+    static const int area = 200;
+    static const double power = 0.25;
+
+    Twos_complement(Latch &input1, Latch& output)
+    {
+        in[0].connection = &input1;
+        out = &output;
+    }
+
+    void receive_clock() { out->before = result;}
+
+    void do_function()
+    {
+        long long a = in[0].connection->after;
+
+        result = ~a;
+        result++;
+    }
+     
+    private:
+    Port in[1];
+    Latch *out;
+
+    long long result;
+};
