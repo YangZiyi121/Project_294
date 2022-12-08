5e9dac15480e7599a086b1f4b6a189394baff1c2
medbzkst <105297323+medbzkst@users.noreply.github.com>
2022-12-08 10:48:11 +0300

    added ops to ALU

--- ALU.cpp
+++ ALU.cpp
@@ -6,15 +6,30 @@
 
 //TODO: Define those codes based on the architectural needs
 
-#define ADDcode
-#define MULTcode
-#define DIVcode
-#define NOTCcode
-#define ANDcode
-#define ORcode
-#define XORcode
-#define SRLcode
-#define SLLcode
+// #define ADDcode
+// #define MULTcode
+// #define DIVcode
+// #define NOTCcode
+// #define ANDcode
+// #define ORcode
+// #define XORcode
+// #define SRLcode
+// #define SLLcode
+
+//FOR TESTING PURPOSES
+
+#define ADDcode 1
+#define ADDcode 2
+#define MULTcode 3
+#define DIVcode 4
+#define NOTCcode 5
+#define ANDcode 6
+#define ORcode 7
+#define XORcode 8
+#define SRLcode 9
+#define SLLcode 10
+#define PASS0code 11
+#define PASS1code 12
 
 
 class ALU : public Device {
@@ -55,6 +70,9 @@ public:
             case ADDcode:
 				ALU::result = ALU::in[0].connection->after + ALU::in[1].connection->after;
                 break;
+            case SUBcode:
+				ALU::result = ALU::in[0].connection->after - ALU::in[1].connection->after;
+                break;
             case MULTcode:
                 ALU::result = ALU::in[0].connection->after * ALU::in[1].connection->after;
                 break;
@@ -79,8 +97,14 @@ public:
 			case SLLcode:
 				ALU::result = ALU::in[0].connection->after << ALU::in[1].connection->after;
                 break;
+			case PASS0code:
+				ALU::result = ALU::in[0].connection->after;
+                break;
+			case PASS1code:
+				ALU::result = ALU::in[1].connection->after;
+                break;
 			default:
-				assert(false); //Dangerous if this code is edited. Never forget to put any break
+				assert(false); //Dangerous if this code is edited. Never forget to put the breaks
 				break;
 			//TODO: Substruction?
         }
@@ -94,28 +118,25 @@ private:
 	Port control; 
 };
 
-// /*Testing*/
-// int main()
-// {
-// 	//Initialize Ports
-// 	Latch latch1, latch2, output;
-// 	latch1.before = 1050;
-// 	latch2.before = 1000;
+/*Testing*/
+int main()
+{
+	//Initialize Ports
+	Latch latch1, latch2, output;
+	Port control; 
 
-// 	//Create device
-// 	ALU ALU(latch1, latch2, output);
+	//Create device
+	ALU ALU(latch1, latch2, output, control);
 
-// 	//Send clk to latches
-// 	latch1.receive_clock();
-// 	latch2.receive_clock();
-// 	output.receive_clock();
+	//ADD TEST
+	std::cout << "ADD TEST" << std::endl;
+	control.before = 1; latch1.before = 100; latch2.before = 254;
+	//Send clk to latches
+	latch1.receive_clock();latch2.receive_clock();control.receive_clock();output.receive_clock();
+	//Propagate data through device
+	ALU.do_function();ALU.receive_clock();
+	//Receive the clk and see the result
+	std::cout << output.before <<std::endl;
 
-// 	//propagate data through device
-// 	ALU.do_function();
-// 	ALU.receive_clock();
-
-// 	//Receive the clk and see the result
-// 	std::cout << output.before <<std::endl;
-
-// }
+}
 
