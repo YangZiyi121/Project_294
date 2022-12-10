b4906d7cf4f1f6dc13d78e2aea60210782888407
YangZiyi121 <ziyi.yang@kaust.edu.sa>
2022-12-10 16:44:21 +0300

    New PC done

--- PC_new.cpp
+++ PC_new.cpp
@@ -3,53 +3,94 @@
 #include "Device.h"
 #include "Port.h"
 
-class Logic : public Device
+//br rd    0b001
+//brr rd   0b010
+//brr L    0b011
+//brnz rd rs 0b100
+//brgt rd rs rt 0b101
+
+class PC_new : public Device
 {
     public:
-    static const int cycles = 1;
-    static const int area = 600;
-    static const double power = 0.75;
 
-    Logic(Latch &ALU, Latch &Rd, Latch &L, Latch &PC, Latch &controlN, Latch &output)
+    PC_new (Latch &ALU, Latch &Rd, Latch &L, Latch &PC, Latch &controlN, Latch &output)
     {
         in[0].connection = &ALU;
         in[1].connection = &Rd;
         in[2].connection = &L;
         in[3].connection = &PC;
-        control.connection = &controlN;
+        control.connection = &controlN; //3-bits
         out = &output;
     }
 
-    void receive_clock() { out->before = result;}
-
-    void receive_clock_latches() 
-    {
-        
+    void receive_clock() { 
+        out->before = result;
     }
 
     void do_function()
     {
-        long long alu = in[0].connection->after; // either 
-        long long Rd = in[1].connection->after;
-        long long L = in[2].connection->after;
-        long long PC = in[3].connection->after;
-        long long z = control.connection->after;
-        
+        switch(control.connection->after){
+            case 0b001:    //br rd
+                result = in[1].connection->after;
+                break;
 
-        if(z == 0)
-            result = ~a;
-        if(z == 1)
-            result = a&b;
-        if(z == 2)
-            result = a|b;
-        if(z == 3)
-            result = a^b;
+            case 0b010:    //brr rd
+                result = in[3].connection->after + in[1].connection->after;
+                break;
+            
+            case 0b011:   //brr L
+                result = in[3].connection->after + in[2].connection->after;
+                break;
+            
+            case 0b100:  //brnz rd rs 
+                if(in[0].connection->after == 0){    //rs = 0
+                    result = in[3].connection->after + 4;
+                }else{
+                    result = in[1].connection->after;
+                }
+                break;
+            case 0b101: //brgt rd rs rt 0b101
+                if(in[0].connection->after > 0){ //rs<=rt
+                    result = in[1].connection->after;
+                }else{
+                    result = in[3].connection->after + 4;
+                }
+
+        }
+        
     }
      
     private:
     Port in[4];
     Latch *out;
     Port control;
-
     long long result;
 };
+
+int main()
+{
+    Latch alu, rd, l, pc, control, output;
+
+    //Initialize latches
+    alu.before = 0; //use this format if you want binary representation
+    rd.before = 200;
+    l.before = 13;
+    pc.before = 89;
+    control.before = 0b101;
+
+    //Create Device
+    PC_new device (alu, rd, l, pc, control, output);
+    
+    //send clock to latches
+    alu.receive_clock();rd.receive_clock();l.receive_clock();pc.receive_clock();control.receive_clock(); output.receive_clock();
+    //std::cout << input1.after <<std::endl;
+
+    //propogate data through device
+    device.do_function();
+    device.receive_clock();
+
+    //result should now be output.before 
+    std::cout << output.before <<std::endl;
+    //std::cout << std::bitset<10>(output.before) <<std::endl;
+    
+}
