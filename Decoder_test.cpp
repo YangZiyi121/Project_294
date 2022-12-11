2041f8409231fade58e2afcea2c3eeccaffec369
rozirr <rayan.rozi@kaust.edu.sa>
2022-12-11 22:43:55 +0300

    commented decoder

--- Decoder_test.cpp
+++ Decoder_test.cpp
@@ -38,7 +38,7 @@ public:
         Decoder::result[1] = stoull("00000000000000000", 0, 2);//mux2 mux3 cycle
         Decoder::result[2] = stoull("00000000000000000", 0, 2);//RF cycle
         Decoder::result[3] = stoull("00000000000000000", 0, 2);//io mux4 cycle 
-        Decoder::result[4] = stoull("00000000001000000", 0, 2);//alu cycle
+        Decoder::result[4] = stoull("00000000000000000", 0, 2);//alu cycle
         Decoder::result[5] = stoull("00000000000000000", 0, 2);//dm cycle
         Decoder::result[6] = stoull("00000000000000001", 0, 2);//mux5 cycle
         Decoder::result[7] = stoull("00110000000000000", 0, 2);//mux2 mux3 WB cycle
@@ -58,6 +58,7 @@ public:
         switch (Decoder::in.connection->after)
         {
         case 0x0://add rd, rs, rt // to have number or opcode in integer number....
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000001000000", 0, 2);//cy 5
             
@@ -65,44 +66,118 @@ public:
 
         case 0x1: //addi
             Decoder::result[3] = stoull("00000010000000000", 0, 2);//cy 4
+            Decoder::result[4] = stoull("00000000001000000", 0, 2);//cy 5
             break;
-        case 0x2:
+        case 0x2://sub rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000010000000", 0, 2);//cy 5
-            break;
 
-        case 0x3:
+            break;
+        case 0x3://subi rd, L
             Decoder::result[3] = stoull("00000010000000000", 0, 2);
             Decoder::result[4] = stoull("00000000010000000", 0, 2);//cy 5
-            break;
 
-        case 0x4:
+            break;
+        case 0x4://mul rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
-            Decoder::result[4] = stoull("00000000011000000", 0, 2);//cy 5                   
+            Decoder::result[4] = stoull("00000000011000000", 0, 2);//cy 5
+                                      
+                                      
             break;
-
-        case 0x5:
+        case 0x5://div rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000100000000", 0, 2);//cy 5
-
+                                     
             break;
-        case 0x6:
+        case 0x6: //and rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000110000000", 0, 2);//cy 5
+
             break;
-        case 0x7:
+        case 0x7://or rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000111000000", 0, 2);//cy 5
-            break;
 
-        case 0x8:
+            break;
+        case 0x8://xor rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000001000000000", 0, 2);//cy 5
-            break;
 
-        case 0x9:
+            break;
+        case 0x9://not rd, rs
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
             Decoder::result[3] = stoull("00000000000000000", 0, 2);
             Decoder::result[4] = stoull("00000000101000000", 0, 2);//cy 5
+            break;
+
+        case 0xa://shftr rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
+            Decoder::result[3] = stoull("00000000000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001001000000", 0, 2);//cy 5
+            break;
+
+        case 0xb://shftri rd, L
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
+            Decoder::result[3] = stoull("00000010000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001001000000", 0, 2);//cy 5
+            break;
+
+        case 0xc://shftl rd, rs, rt
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
+            Decoder::result[3] = stoull("00000000000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001010000000", 0, 2);//cy 5
+            break;
+
+        case 0xd://shftli rd, L
+            Decoder::result[2] = stoull("00001000000000000", 0, 2);//RF cycle
+            Decoder::result[3] = stoull("00000010000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001010000000", 0, 2);//cy 5
+            break;
+
+        case 0x15: //mov rd, (rs)(L)
+            Decoder::result[0] = stoull("01000000000000000", 0, 2);
+            Decoder::result[3] = stoull("00000010000000000", 0, 2);
+            Decoder::result[4] = stoull("00000000001000000", 0, 2);//cy 5
+            Decoder::result[5] = stoull("00000000000000100", 0, 2);//cy 5
+            Decoder::result[6] = stoull("00000000000000010", 0, 2);//cy 5
+            break;
+
+        case 0x16://mov rd, rs
+            Decoder::result[3] = stoull("00000000000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001011000000", 0, 2);//cy 5
+            break;
+
+        case 0x17: //mov rd, L
+            Decoder::result[3] = stoull("00000010000000000", 0, 2);
+            Decoder::result[4] = stoull("00000001100000000", 0, 2);//cy 5
+            break;
+
+        case 0x18: //mov (rd)(L), rs
+            Decoder::result[0] = stoull("01000000000000000", 0, 2);
+            Decoder::result[2] = stoull("00000000000000000", 0, 2);
+            Decoder::result[3] = stoull("00000001100000000", 0, 2);
+            Decoder::result[4] = stoull("00000000000001000", 0, 2);//cy 5
+
+            Decoder::result[5] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[6] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[7] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[8] = stoull("00000000000000000", 0, 2);//cy 5
+            break;
+
+        case 0x1d: //in rd, rs
+            Decoder::result[0] = stoull("01000000000000000", 0, 2);//cy 1           
+            Decoder::result[3] = stoull("00000000000010000", 0, 2);//cy 4
+            Decoder::result[4] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[5] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[6] = stoull("00000000000000000", 0, 2);//cy 5
+            Decoder::result[7] = stoull("00110000000000000", 0, 2);//cy 8
+            Decoder::result[8] = stoull("00001100000000000", 0, 2);//cy 9
             break;
 
         case 0x1e:
